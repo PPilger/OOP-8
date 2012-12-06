e3472cf508d6024e95d4ad04ce0159dbecdf4181
PPilger <peter@pilgerstorfer.at>
2012-12-06 21:31:33 +0100

    sdf

--- src/Tractor.java
+++ src/Tractor.java
@@ -1,22 +1,21 @@
-
 public class Tractor<E extends Engine<F>, F extends Number> {
 	private static int idCounter = 0;
 	private final int id;
 	private E engine;
 	private F totalConsumption;
 	private int operatingHours;
-	
+
 	public Tractor(E engine) {
 		this.id = idCounter;
 		idCounter++;
-		
+
 		this.engine = engine;
-		
 	}
 
 	public int incOperatingHours(int addHours) {
-		if (addHours > 0)
+		if (addHours > 0) {
 			this.operatingHours += addHours;
+		}
 		return this.operatingHours;
 	}
 
