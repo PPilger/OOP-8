7a08b6fe11f1d127e784130550f6d62383e07e24
PPilger <peter@pilgerstorfer.at>
2012-12-07 01:32:34 +0100

    dynamisches avg

---
+++ src/Calc.java
@@ -0,0 +1,5 @@
+
+public interface Calc {
+	public Number inc(Number num, Tractor tr);
+	public Number avg(Number sum, Integer count);
+}
--- src/Farm.java
+++ src/Farm.java
@@ -1,80 +1,116 @@
+import java.lang.reflect.Method;
+
 public class Farm {
 
-	private Map<Integer, Tractor> myTractors;//Todo: Pilgis Collection
+	private Map<Integer, Tractor> myTractors;// Todo: Pilgis Collection
 	private final String name;
-	
-	public Farm(String name)
-	{
+
+	public Farm(String name) {
 		this.name = name;
 		this.myTractors = new Map<Integer, Tractor>();
 	}
-	
-	public void addTractor(Tractor tr)
-	{
+
+	public void addTractor(Tractor tr) {
 		this.myTractors.put(tr.getID(), tr);
 	}
-	
-	public void removeTractor(int id)
-	{
+
+	public void removeTractor(int id) {
 		myTractors.remove(id);
 	}
-	
-	public void changeRole(int id, TractorRole tr)
-	{
+
+	public void changeRole(int id, TractorRole tr) {
 		myTractors.get(id);
 	}
-	
-	public Map<String, Double> avgOperatingHours()
-	{
+
+	private void avg(Method identifier, Calc calc, Map<Object, Number> target) {
+		Iterator<Tractor> it = myTractors.iterator();
+		Iterator<Object> keyIter;
+		Map<Object, Integer> counter = new Map<Object, Integer>();
+
+		keyIter = target.keyIterator();
+		while (keyIter.hasNext()) {
+			Object key = keyIter.next();
+			counter.put(key, 0);
+		}
+
+		while (it.hasNext()) {
+			Tractor tr = it.next();
+			Object o = m.invoke(tr);
+
+			while (keyIter.hasNext()) {
+				Object key = keyIter.next();
+				if (o == key) {
+					Number sum = target.get(key);
+					Integer count = counter.get(key);
+
+					sum = calc.inc(sum, tr);
+					count++;
+
+					target.put(key, sum);
+					counter.put(key, count);
+				}
+			}
+		}
+
+		keyIter = target.keyIterator();
+		while (keyIter.hasNext()) {
+			Object key = keyIter.next();
+
+			Number sum = target.get(key);
+			Integer count = counter.get(key);
+
+			Number avg = calc.avg(sum, count);
+
+			target.put(key, avg);
+		}
+	}
+
+	public Map<String, Double> avgOperatingHours() {
 		double sumSeeder = 0;
 		double sumFertilizer = 0;
 		int cntSeeder = 0;
 		int cntFertilizer = 0;
-		
+
 		Iterator<Tractor> it = myTractors.iterator();
-		
-		while(it.hasNext()) {
+
+		while (it.hasNext()) {
 			Tractor tr = it.next();
-			if(tr.getRole().getClass() == Seeder.class) {
+			if (tr.getRole().getClass() == Seeder.class) {
 				sumSeeder += tr.getOperatingHours();
 				cntSeeder += 1;
-			} else if(tr.getRole().getClass() == Fertilizer.class) {
+			} else if (tr.getRole().getClass() == Fertilizer.class) {
 				sumFertilizer += tr.getOperatingHours();
 				cntFertilizer += 1;
 			}
 		}
-		
+
 		Map<String, Double> result = new Map<String, Double>();
-		result.put("All", ((sumSeeder+sumFertilizer)/(cntSeeder+cntFertilizer)));
-		result.put("Seeder", sumSeeder/cntSeeder);
-		result.put("Fertilizer", sumFertilizer/cntFertilizer);
-		
+		result.put("All",
+				((sumSeeder + sumFertilizer) / (cntSeeder + cntFertilizer)));
+		result.put("Seeder", sumSeeder / cntSeeder);
+		result.put("Fertilizer", sumFertilizer / cntFertilizer);
+
 		return result;
 	}
-	
-	public double avgDieselUsage()
-	{
+
+	public double avgDieselUsage() {
 		return 0;
 	}
-	
-	public double avgBioGasUsage()
-	{
+
+	public double avgBioGasUsage() {
 		return 0;
 	}
-	
-	public int minCoulterCount()
-	{
+
+	public int minCoulterCount() {
 		return 0;
 	}
-	
-	public int maxCoulterCount()
-	{
+
+	public int maxCoulterCount() {
 		return 0;
 	}
-	
-	public double avgCapacity()
-	{
+
+	public double avgCapacity() {
 		return 0;
 	}
-	
+
 }
