dd31bc3760295a1ac8e7486911c3e3840ae73f5e
koegleal-tu <koegleal_foren@hotmail.com>
2012-12-07 23:07:43 +0100

    FuelDistr

---
+++ src/FuelDistributor.java
@@ -0,0 +1,15 @@
+
+public class FuelDistributor<V> extends Distributor<Tractor, Object, V>{
+
+	public FuelDistributor(Merger<Tractor, V> comb) {
+		super(comb);
+		addType(Diesel.class);
+		addType(BioGas.class);
+	}
+
+	@Override
+	protected boolean fitsKey(Tractor obj, Object fuel) {
+		return obj.getClass() == fuel;
+	}
+
+}
