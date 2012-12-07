12bd4dc46f806e60640d8704d3ae82509105cc7f
Peter Pilgerstorfer <peter@pilgerstorfer.at>
2012-12-08 00:32:28 +0100

    vereinfachen

--- src/CapacityMerger.java
+++ src/CapacityMerger.java
@@ -1,15 +1,5 @@
+public class CapacityMerger implements Merger<Tractor, Double> {
 
-public class CapacityMerger implements Merger<Tractor, Double>{
-
-	private Combinator<Double, Double, Double> comb =
-			new Combinator<Double, Double, Double>() {
-		
-		@Override
-		public Double combine(Double val1, Double val2) {
-			return val1 + val2;
-		}
-	};
-	
 	@Override
 	public Double initialValue() {
 		return 0.;
@@ -17,9 +7,8 @@ public class CapacityMerger implements Merger<Tractor, Double>{
 
 	@Override
 	public Double merge(Tractor newElem, Double value) {
-		if(newElem.getRole().getClass() == Fertilizer.class)
-		{
-			return comb.combine(value, newElem.getAttirbute().doubleValue());
+		if (newElem.getRole().getClass() == Fertilizer.class) {
+			return value + newElem.getAttirbute().doubleValue();
 		}
 		return value;
 	}
