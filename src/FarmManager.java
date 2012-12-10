d1ef71934ba7a18376eac222108735238a9c4d3e
koegleal-tu <koegleal_foren@hotmail.com>
2012-12-11 00:24:15 +0100

    Kommentare Annotationen

--- src/FarmManager.java
+++ src/FarmManager.java
@@ -1,13 +1,20 @@
+/**
+ * Manages farms/tractors, each of them will be unique by their name/id
+ * 
+ * @author Koegler Alexander
+ */
+@Author("Koegler Alexander")
 public class FarmManager {
 	
 	private Map<String, Farm> myFarms = new Map<String, Farm>();
 	private Map<Integer, Tractor> allTractors = new Map<Integer, Tractor>();
 	
-	public FarmManager()
-	{
-		
-	}
-	
+	/**
+	 * Adds unique farm
+	 * @param newFarm
+	 * @return false if farm already exists
+	 */
+	@Author("Koegler Alexander")
 	public boolean addFarm(Farm newFarm)
 	{
 		if(myFarms.get(newFarm.getName()) != null)
@@ -21,7 +28,13 @@ public class FarmManager {
 	}
 	
 	
-	
+	/**
+	 * Adds unique tractor to existing farm
+	 * @param farm
+	 * @param newTractor
+	 * @return false if tractor already exists, or farm doesn't
+	 */
+	@Author("Koegler Alexander")
 	public boolean addTractor(String farm, Tractor newTractor)
 	{
 		if(myFarms.get(farm) == null || allTractors.get(newTractor.getID()) != null)
@@ -35,6 +48,12 @@ public class FarmManager {
 		return true;
 	}
 	
+	/**
+	 * Removes tractor from any farm
+	 * @param id of the tractor
+	 * @return false if there is no such tractor with this id
+	 */
+	@Author("Koegler Alexander")
 	public boolean removeTractor(int id)
 	{
 		if(allTractors.get(id) == null)
@@ -52,16 +71,13 @@ public class FarmManager {
 		return false;		
 	}
 	
-	public Map<String, Farm> getFarms()
-	{
-		return myFarms;		
-	}
-	
+	/**
+	 * Returns an existing farm by its unique name
+	 * @return farm; null if there's no such farm
+	 */
+	@Author("Koegler Alexander")
 	public Farm getFarm(String name)
 	{
-		return myFarms.get(name);
+		return myFarms.get(name);		
 	}
-	
-	
-
 }
