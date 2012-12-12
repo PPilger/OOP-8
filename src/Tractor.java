0c2941f59b9f25b59b67449f07d6f2b2cb56d93e
Peter Pilgerstorfer <peter@pilgerstorfer.at>
2012-12-12 01:58:56 +0100

    tractor

--- src/Tractor.java
+++ src/Tractor.java
@@ -1,7 +1,4 @@
 @Author("Pilgerstorfer Peter")
-/*
- * In this class a tractor is discribed.
- */
 public class Tractor<F extends Fuel<T>, T extends Number> {
 	private final int id;
 	private F totalConsumption;
@@ -25,11 +22,17 @@ public class Tractor<F extends Fuel<T>, T extends Number> {
 		return this.role;
 	}
 
+	/**
+	 * @return the total consumption
+	 */
 	@Author("Pilgerstorfer Peter")
 	public Fuel getFuel() {
 		return this.totalConsumption;
 	}
 
+	/**
+	 * Increments the operating hours by the specified amount.
+	 */
 	@Author("Pilgerstorfer Peter")
 	public void incOperatingHours(int amount) {
 		if (amount > 0) {
@@ -42,21 +45,35 @@ public class Tractor<F extends Fuel<T>, T extends Number> {
 		return operatingHours;
 	}
 
+	/**
+	 * Increments the total consumption by the specified amount. The amount has
+	 * to fit to the Fuel-type of the Tractor.
+	 */
 	@Author("Pilgerstorfer Peter")
-	public void incConsumption(T fuelAmount) {
-		totalConsumption.incAmount(fuelAmount);
+	public void incConsumption(T amount) {
+		totalConsumption.incAmount(amount);
 	}
 
+	/**
+	 * Returns the total consumption. The returned value's type depends on the
+	 * Fuel-type of the Tractor.
+	 */
 	@Author("Pilgerstorfer Peter")
 	public T getConsumption() {
 		return totalConsumption.getAmount();
 	}
 
+	/**
+	 * Changes the role of the tractor to the new one.
+	 */
 	@Author("Pilgerstorfer Peter")
 	public void changeRole(TractorRole newRole) {
 		role = newRole;
 	}
 
+	/**
+	 * Returns the attribute of the current role.
+	 */
 	@Author("Pilgerstorfer Peter")
 	public Number getAttirbute() {
 		return role.getAttribute();
